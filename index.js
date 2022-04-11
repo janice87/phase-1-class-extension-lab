9483556fd553a25147261afb0d38fa49afe3f86b
Janice Chan <janice.chan1288@gmail.com>
2022-04-11 07:46:52 -0400

    commit tree 94db65edb128f5eed2b4692ea5a1412d93484941

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+class Polygon {
+    constructor(side) {
+        this.side = side;
+        this.count = this.side.length;
+    }
+    get countSides () {
+        return this.side.length;
+    }
+    get perimeter () {
+        if(!Array.isArray(this.side)) return; //Array.isArray method determines if the passed value is an array
+        let sum = 0;
+        for(let oneSide of this.side) {
+            sum += oneSide
+        }
+        return sum;
+    } 
+}
+
+class Triangle extends Polygon {
+    get isValid () {
+        if(!Array.isArray(this.side)) return;
+        if (this.count !== 3) return;
+        let side1 = this.side[0]
+        let side2 = this.side[1]
+        let side3 = this.side[2]
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) 
+          
+    }
+}
+
+class Square extends Polygon {
+    get isValid () {
+        if(!Array.isArray(this.side)) return;
+        if(this.count !== 4) return;
+        let side1 = this.side[0]
+        let side2 = this.side[1]
+        let side3 = this.side[2]
+        let side4 = this.side[3]
+
+        return ((side1 === side2) && (side1 === side3) && (side1 === side4)) 
+    }
+
+    get area() {
+        if(!Array.isArray(this.side)) return;
+        if(this.count !== 4) return;
+        return this.side[0] * this.side[0]
+    }
+}
